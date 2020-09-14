38b0a4f6b72d23ffd70c3d0d8843b8a6373d0762
sergeycin <67867645+sergeycin@users.noreply.github.com>
2020-09-14 16:33:49 +0300

    Add files via upload

---
+++ int/js/index.js
@@ -0,0 +1,119 @@
+'use strict';
+
+var $uiAccordion = $('.js-ui-accordion');
+
+$uiAccordion.accordion({
+  collapsible: true,
+  heightStyle: 'content',
+
+  activate: function activate(event, ui) {
+    var newHeaderId = ui.newHeader.attr('id');
+
+    if (newHeaderId) {
+      history.pushState(null, null, '#' + newHeaderId);
+    }
+  },
+
+  create: function create(event, ui) {
+    var $this = $(event.target);
+    var $activeAccordion = $(window.location.hash);
+
+    if ($this.find($activeAccordion).length) {
+      $this.accordion('option', 'active', $this.find($this.accordion('option', 'header')).index($activeAccordion));
+    }
+  }
+});
+
+$(window).on('hashchange', function (event) {
+  var $activeAccordion = $(window.location.hash);
+  var $parentAccordion = $activeAccordion.parents('.js-ui-accordion');
+
+  if ($activeAccordion.length) {
+    $parentAccordion.accordion('option', 'active', $parentAccordion.find($uiAccordion.accordion('option', 'header')).index($activeAccordion));
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+'use strict';
+
+var $uiAccordion = $('.js-ui-accordion2');
+
+$uiAccordion.accordion({
+  collapsible: true,
+  heightStyle: 'content',
+
+  activate: function activate(event, ui) {
+    var newHeaderId = ui.newHeader.attr('id');
+
+    if (newHeaderId) {
+      history.pushState(null, null, '#' + newHeaderId);
+    }
+  },
+
+  create: function create(event, ui) {
+    var $this = $(event.target);
+    var $activeAccordion = $(window.location.hash);
+
+    if ($this.find($activeAccordion).length) {
+      $this.accordion('option', 'active', $this.find($this.accordion('option', 'header')).index($activeAccordion));
+    }
+  }
+});
+
+$(window).on('hashchange', function (event) {
+  var $activeAccordion = $(window.location.hash);
+  var $parentAccordion = $activeAccordion.parents('.js-ui-accordion2');
+
+  if ($activeAccordion.length) {
+    $parentAccordion.accordion('option', 'active', $parentAccordion.find($uiAccordion.accordion('option', 'header')).index($activeAccordion));
+  }
+});
+
+
+
+
+
+'use strict';
+
+var $uiAccordion = $('.js-ui-accordion3');
+
+$uiAccordion.accordion({
+  collapsible: true,
+  heightStyle: 'content',
+
+  activate: function activate(event, ui) {
+    var newHeaderId = ui.newHeader.attr('id');
+
+    if (newHeaderId) {
+      history.pushState(null, null, '#' + newHeaderId);
+    }
+  },
+
+  create: function create(event, ui) {
+    var $this = $(event.target);
+    var $activeAccordion = $(window.location.hash);
+
+    if ($this.find($activeAccordion).length) {
+      $this.accordion('option', 'active', $this.find($this.accordion('option', 'header')).index($activeAccordion));
+    }
+  }
+});
+
+$(window).on('hashchange', function (event) {
+  var $activeAccordion = $(window.location.hash);
+  var $parentAccordion = $activeAccordion.parents('.js-ui-accordion3');
+
+  if ($activeAccordion.length) {
+    $parentAccordion.accordion('option', 'active', $parentAccordion.find($uiAccordion.accordion('option', 'header')).index($activeAccordion));
+  }
+});
