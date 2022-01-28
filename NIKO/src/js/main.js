10f32c3971cf3b7550250a500a42bd5095167c8c
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-01-28 11:06:09 +0300

    Add files via upload

---
+++ NIKO/src/js/main.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+
+//     particlesJS.load('particles-js', '/particles/particles.json', function() {
+//         console.log('callback - particles.js config loaded');
+//         console.log('ok')
+//       });
+
+// });
