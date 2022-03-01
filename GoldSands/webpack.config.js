83cc36a9ee4fd461dc61ee9a6aec13eb188c6e7e
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-03-01 16:12:42 +0300

    stack

---
+++ GoldSands/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/assets/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './src/assets/dist')
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+              },
+        ]
+    }
+}
