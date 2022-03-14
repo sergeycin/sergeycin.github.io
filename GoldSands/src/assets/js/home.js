fdcb4ddf3bc493244f74324d996bf5b303a048e2
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-03-14 21:54:16 +0300

    eventlistner

--- GoldSands/src/assets/js/home.js
+++ GoldSands/src/assets/js/home.js
@@ -6,10 +6,14 @@ $(document).ready(function(){
     if ($(window).width() > '868') {
 
         $('.home__header').addClass('hed-hov')
-       document.querySelector('.hed-hov').onmouseover = (event) =>{
-            console.log('home-head')
+       document.querySelector('.hed-hov').addEventListener('mouseover', myclick)
+       
+       function myclick (){
+           console.log('head-home')
+       }
+        document.querySelector('.compas').onclick = () =>{
+            document.querySelector('.hed-hov').removeEventListener('mouseover', myclick)
         }
-
         
     }
     else{
