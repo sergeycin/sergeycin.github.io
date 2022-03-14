e14c3a643d6290756ad340a8037adf84b283bd32
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-03-14 22:18:19 +0300

    js begin home

--- GoldSands/src/assets/js/home.js
+++ GoldSands/src/assets/js/home.js
@@ -1,19 +1,31 @@
 console.log('home')
 
 
+var korpus = 0;
+var stage = 1;
+var scheme = 2;
+var  room = 3;
+
+
+var state = korpus; // Текущее состояние 
+
 $(document).ready(function(){
 
     if ($(window).width() > '868') {
 
-        $('.home__header').addClass('hed-hov')
-       document.querySelector('.hed-hov').addEventListener('mouseover', myclick)
+
+        
+    //     $('.home__header').addClass('hed-hov')
+    //    document.querySelector('.hed-hov').addEventListener('mouseover', ()=>{
+    //     console.log('head-home')
+    //    })
        
-       function myclick (){
-           console.log('head-home')
-       }
-        document.querySelector('.compas').onclick = () =>{
-            document.querySelector('.hed-hov').removeEventListener('mouseover', myclick)
-        }
+    // //    function myclick (){
+    // //        console.log('head-home')
+    // //    }
+    //     document.querySelector('.compas').onclick = () =>{
+    //         document.querySelector('.hed-hov').removeEventListener('mouseover',()=>{})
+    //     }
         
     }
     else{
@@ -21,3 +33,12 @@ $(document).ready(function(){
     }
 
 });
+
+
+
+
+function hoverHome (homeObject,numberHone){
+    homeObject.addEventListener('mouseover', ()=>{
+        console.log('head-home')
+       })
+}
